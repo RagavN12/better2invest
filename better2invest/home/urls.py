382a4748c1b977cfd01e06b9cf018bed85514179
RagavN12 <sriragav@protonmail.com>
2025-06-25 16:02:03 -0400

    created login and signup pages

--- better2invest/home/urls.py
+++ better2invest/home/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.mainpage),
-    path('home/', views.mainpage)
+    path('home/', views.mainpage),
+    path("login/", views.loginpage, name="login"),
+    path("signup/",views.signupPage, name="signup")
 ]
--- better2invest/home/views.py
+++ better2invest/home/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def mainpage(request):
     return render(request, "home.html")
+
+def loginpage(request):
+    return render(request, "login.html");
+
+def signupPage(request):
+    return render(request, "signup.html")
